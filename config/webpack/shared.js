17e992d8ef46892657de33ca9a7d8431d607ab2a
Rajib Ahmed <rajib.ahmed@valtech.se>
2017-07-19 13:47:02 +0200

    Working almost

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -10,6 +10,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const extname = require('path-complete-extname')
 const { env, settings, output, loadersDir } = require('./configuration.js')
+const appConfig = require('./app-config.js')
 
 const extensionGlob = `**/*{${settings.extensions.join(',')}}*`
 const entryPath = join(settings.source_path, settings.source_entry_path)
@@ -36,14 +37,14 @@ module.exports = {
     rules: sync(join(loadersDir, '*.js')).map(loader => require(loader))
   },
 
-  plugins: [
-    new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
-    new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
-    new ManifestPlugin({
-      publicPath: output.publicPath,
-      writeToFileEmit: true
-    })
-  ],
+	plugins: [
+		new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
+		new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
+		new ManifestPlugin({
+			publicPath: output.publicPath,
+			writeToFileEmit: true
+		})
+	],
 
 	resolve: {
 		alias: {
